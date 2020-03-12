f02c35a36d8e45e3970683ef268c65ced39a2ead
guojunwei <840126489@qq.com>
2020-03-13 00:18:13 +0800

    build: 删除bin文件夹

--- bin/react-frame-cli.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const commander = require('commander');
-const program = new commander.Command();
-
-program.version(require('../package.json').version);
-
-program.parse(process.argv);
-  
