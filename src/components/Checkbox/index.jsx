46220e1af3604e6560b0fd3d308355311eba2f5d
郭军伟 <15037163033@163.com>
2020-03-20 17:42:29 +0800

    feat: add CheckoutForm

---
+++ src/components/Checkbox/index.jsx
@@ -0,0 +1,20 @@
+import BaseForm from '../BaseForm';
+import { Checkout } from 'antd';
+
+class CheckoutForm extends BaseForm {
+  getComponent() {
+    const {
+
+    } = this.props;
+  }
+}
+
+CheckoutForm.propTypes = {
+
+}
+
+CheckoutForm.defaultProps = {
+
+}
+
+export default CheckoutForm;
