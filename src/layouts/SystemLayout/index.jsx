3767362d0b8a825d16b64a3fb84cd5ff83cd97d3
郭军伟 <15037163033@163.com>
2020-04-01 18:04:17 +0800

    feat: 完成页面布局

--- src/layouts/SystemLayout/index.jsx
+++ src/layouts/SystemLayout/index.jsx
@@ -1,7 +1,7 @@
-import { Layout, Menu, Icon } from 'antd';
+import { Layout, Menu, Icon, Breadcrumb } from 'antd';
 import MenuItem from 'antd/lib/menu/MenuItem';
 import SubMenu from 'antd/lib/menu/SubMenu';
-const { Header, Sider, Content, Footer } = Layout;
+const { Header, Sider, Content } = Layout;
 
 class SystemLayout extends Component {
     constructor(props) {
@@ -11,37 +11,38 @@ class SystemLayout extends Component {
     render() {
         return (
             <Layout>
-                <Header>
-                    <HeaderLayout>
-                        <Logo></Logo>
-                        <Menu theme='dark' mode='horizontal' style={{ lineHeight: '64px' }}>
-                            <MenuItem key='1'>nav 1</MenuItem>
-                            <MenuItem key='2'>nav 2</MenuItem>
-                            <MenuItem key='3'>nav 3</MenuItem>
-                        </Menu>
-                    </HeaderLayout>
-
-                </Header>
+                <HeaderLayout>
+                    <Logo></Logo>
+                    <Menu theme='dark' mode='horizontal' style={{ lineHeight: '64px' }}>
+                        <MenuItem key='1'>nav 1</MenuItem>
+                        <MenuItem key='2'>nav 2</MenuItem>
+                        <MenuItem key='3'>nav 3</MenuItem>
+                    </Menu>
+                </HeaderLayout>
                 <Layout>
-                    <Sider width={200} style={{ background: '#fff' }}>
-                        <SiderLayout>
-                            <Menu mode='inline' style={{ height: '100%' }}>
-                                <SubMenu title={
-                                    <span>
-                                        <Icon type="mail" />
-                                        <span>Navigation One</span>
-                                    </span>
-                                }>
-                                    <MenuItem key='1'>option1</MenuItem>
-                                    <MenuItem key='2'>option1</MenuItem>
-                                    <MenuItem key='3'>option1</MenuItem>
-                                    <MenuItem key='4'>option1</MenuItem>
-                                </SubMenu>
-                            </Menu>
-                        </SiderLayout>
-
-                    </Sider>
-                    <Content>content</Content>
+                    <SiderLayout>
+                        <Menu mode='inline' style={{ height: '100%' }}>
+                            <SubMenu title={
+                                <span>
+                                    <Icon type="mail" />
+                                    <span>Navigation One</span>
+                                </span>
+                            }>
+                                <MenuItem key='1'>option1</MenuItem>
+                                <MenuItem key='2'>option1</MenuItem>
+                                <MenuItem key='3'>option1</MenuItem>
+                                <MenuItem key='4'>option1</MenuItem>
+                            </SubMenu>
+                        </Menu>
+                    </SiderLayout>
+                    <ContentLayout>
+                        <BreadcrumbWrap>
+                            <Breadcrumb.Item>Home</Breadcrumb.Item>
+                            <Breadcrumb.Item>Application Center</Breadcrumb.Item>
+                            <Breadcrumb.Item>Application List</Breadcrumb.Item>
+                            <Breadcrumb.Item>An Application</Breadcrumb.Item>
+                        </BreadcrumbWrap>
+                    </ContentLayout>
                 </Layout>
             </Layout>
         )
@@ -50,24 +51,43 @@ class SystemLayout extends Component {
 
 export default SystemLayout;
 
+const siderWidth = '200px';
+const headerHeight = '64px';
+
 const Logo = styled.div`
     width: 120px;
     height: 31px;
     background: rgba(255, 255, 255, 0.2);
-    margin: 16px 28px 16px 28px;
+    margin: 16px 28px 16px 0;
     float: left;
 `
 
-const HeaderLayout = styled.div`
+const HeaderLayout = styled(Header)`
     position:fixed;
     top: 0;
     left: 0;
-    height: 64px
+    right: 0;
+    height: ${headerHeight};
+    background: '#fff';
 `
 
-const SiderLayout = styled.div`
+const SiderLayout = styled(Sider)`
     position: fixed;
-    top: 64px;
+    top: ${headerHeight};
     left: 0;
     bottom: 0;
+    width: ${siderWidth};
+`
+
+const ContentLayout = styled(Content)`
+    position: fixed;
+    top: ${headerHeight};
+    left: ${siderWidth};
+    right: 0;
+    bottom: 0;
+`
+
+const BreadcrumbWrap = styled(Breadcrumb)`
+    margin: 16px 0 16px 16px;
+
 `
